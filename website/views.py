6b1d6559d702857ba1d65e8562f96ede1fab078c
jcarcamo <carcamo1cruz@gmail.com>
2025-10-11 09:12:59 -0600

    feat: set up service detail page with slug-based URL pattern, built view using get_object_or_404 to render service, added area-specific menu to display services on click, & created servicio.html to verify functionality

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import AreaServicio
+from .models import AreaServicio, ServicioDetalle
 
 
 def home(request):
@@ -23,3 +23,10 @@ def areas(request):
         "areas_juridicas": areas_juridicas,
     }
     return render(request, "website/servicios.html", context)
+
+
+def servicios(request, servicio_slug):
+    """handles the request & sends information about a specific service to a template"""
+    servicio = get_object_or_404(ServicioDetalle, slug=servicio_slug)
+    context = {"servicio": servicio}
+    return render(request, "website/servicio.html", context)
