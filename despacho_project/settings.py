0e33f82c41c9562ba8092b92cca60515c439f89d
jcarcamo <carcamo1cruz@gmail.com>
2025-10-15 09:51:18 -0600

    fix: corrected template tags, URL namespaces, and added django_extensions to settings in order to use Shell_plus

--- despacho_project/settings.py
+++ despacho_project/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tailwind",
     "theme",
+    "django_extensions",
 ]
 
 if DEBUG:
--- services/urls.py
+++ services/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "services"
 urlpatterns = [
     path("servicios/", views.areas, name="servicios"),
-    path("servicio/<slug:servicio_slog>/", views.servicios, name="servicio"),
+    path("servicio/<slug:servicio_slug>/", views.servicios, name="servicio"),
 ]
