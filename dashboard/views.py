2e88e45e5576f7efd4280b0c9e0cd2d822d83c21
jcarcamo <carcamo1cruz@gmail.com>
2025-10-30 12:43:09 -0600

    refactor(login): removed redundant authenticate function since AuthenticateForm is alreadyused; passed form to login.html & displayed form.errors; update language settings, renamed URL & link label for admin panel in navbar"

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
 from django.contrib import messages
 from django.http import HttpResponse
 
@@ -15,21 +15,11 @@ def dashboard_login(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = request.POST["username"]
-            password = request.POST["password"]
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                if user.is_staff:
-                    login(request, user)
-                    return redirect("dashboard:panel")
-                else:
-                    messages.error(
-                        request, "Acceso denegado. Solo para personal autorizado."
-                    )
-            else:
-                messages.error(request, "Usuario o contraseña incorrectos.")
-        else:
-            messages.error(request, "Usuario o contraseña incorrectos.")
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f"¡Bienvenido de nuevo, {user.username}!")
+            return redirect("dashboard:panel")
+        # messages.error(request, "Usuario o contraseña incorrectos.")
     else:
         form = AuthenticationForm()
 
--- despacho_project/urls.py
+++ despacho_project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("", include("core.urls")),
     path("citas/", include("citas.urls")),
     path("servicios/", include("services.urls")),
-    path("dashboard/", include("dashboard.urls")),
+    path("admin-panel/", include("dashboard.urls")),
 ]
 if settings.DEBUG:
     # Include django_browser_reload URLs only in DEBUG mode
